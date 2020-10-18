2bec47ea231055d3e0204af21c6d94b2356fe3b0
Eduardo Nicoletti Borges <eduardonicoletti@mertins.com.br>
2020-10-17 21:11:06 -0300

    28

--- Semana 1/28/exercicio28.c
+++ Semana 1/28/exercicio28.c
@@ -1,26 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
-    char *name;
-    int i;
-}pBuffer;
+    char name[40];
+}pessoa;
+
+typedef struct
+{
+    int i,qnt;
+}vars;
+
+void *pBuffer;
 
 int main(){
-    
-    pBuffer *p;
-    p = (pBuffer *)malloc(sizeof(pBuffer));
-    if(!p){
-        printf("Memória Insuficiente!");
-        system("pause");
+
+    vars *variaveis;
+    pBuffer = malloc(sizeof(vars));
+    variaveis = pBuffer;
+    variaveis->qnt = 0;
+    if(pBuffer == NULL){
+        printf("Memoria insuficiente!");
         return 1;
     }
-    while(menu(p) == 1)
-    free(p);
+    while(menu(variaveis) == 1);
+    free(pBuffer);
     return 0;
 }
 
-int menu(pBuffer *p)
+void addName(vars *variaveis){
+    pessoa *p;
+    variaveis->qnt++;
+    pBuffer = realloc(pBuffer,sizeof(vars) + (sizeof(pessoa) * variaveis->qnt));
+    variaveis = pBuffer;
+    p = pBuffer + sizeof(vars) + (sizeof(pessoa) * (variaveis->qnt - 1));
+    fflush(stdin);
+    printf("Insira um nome:"); fgets(p->name,40,stdin);
+    printf("%s Inserido!",p->name);
+    pause();
+    return;
+}
+
+void list(vars *variaveis){
+    pessoa *p;
+    p = pBuffer + sizeof(vars);
+    variaveis = pBuffer;
+    for ( variaveis->i = 0; variaveis->i < variaveis->qnt; variaveis->i++)
+    {
+        printf("%i - %s", variaveis->i, p->name);
+        p++;
+    }
+    pause();
+    return;
+}
+
+void removeName(vars *variaveis){
+
+    pessoa *p;
+    printf("Insira o numero do nome a ser removido:"); scanf("%d", variaveis->i);
+    p = pBuffer + sizeof(vars) + (sizeof(pessoa) * variaveis->i);
+    //memcpy( Destino(Pessoa Selecionada), Fonte(Proxima pessoa), Tamanho(Quantidade de pessoas depois da removida) )
+    memcpy(p, p + sizeof(pessoa), sizeof(pessoa) * (variaveis->qnt - variaveis->i));
+    variaveis->qnt--;
+    pBuffer = realloc(pBuffer, sizeof(vars) + (sizeof(pessoa) * variaveis->qnt) );
+    printf("Removido!");
+
+}
+
+void pause(){
+    printf("Pressione enter para continuar!");
+    getchar();
+    fflush(stdin);
+    return;
+}
+
+int menu(vars *variaveis)
 {
     system("cls");
     printf("1) Adicionar nome\n");
@@ -32,15 +86,15 @@ int menu(pBuffer *p)
     {
     case '1':
         fflush(stdin);
-        addName(p);
+        addName(variaveis);
         break;
     case '2':
         fflush(stdin);
-        printf("Ainda n pai");
+        removeName(variaveis);
         break;
     case '3':
         fflush(stdin);
-        printf("Ainda n pai");
+        list(variaveis);
         break;
     case '4':
         return 0;
@@ -52,9 +106,4 @@ int menu(pBuffer *p)
         break;
     }
     return 1;
-}
-
-void addName(pBuffer *p){
-    printf("Insira um nome: "); scanf("%s",&p->name);
-    return;
 }
