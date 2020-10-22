439d103a928b60964e5184cea83a133ad717383b
Eduardo Nicoletti Borges <eduardonicoletti@mertins.com.br>
2020-10-21 21:19:21 -0300

    exercicio27 fix

--- Semana 2/Aula 2/27/exercicio27.c
+++ Semana 2/Aula 2/27/exercicio27.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
-char nome[30];
-int idade;
-int altura;
-}Pessoa;
+typedef struct
+{
+    char nome[30];
+    int idade;
+    int altura;
+} Pessoa;
 
 int main()
 {
@@ -18,32 +19,37 @@ int main()
         printf("Deseja inserir um novo nome?\n1 - Sim\n0 - Nao\n");
         continuar = getchar();
         fflush(stdin);
-        if(continuar == '1'){
+        if (continuar == '1')
+        {
             qnt++;
-            p = realloc(p,qnt * sizeof(Pessoa));
-            if(p == NULL){
-                printf("Memoria Insuficiente!");
+            p = realloc(p, qnt * sizeof(Pessoa));
+            if (p == NULL)
+            {
+                printf("Memoria Insuficiente!\n");
                 system("pause");
-                free(p);
                 return 1;
             }
-            printf("Insira um nome: "); scanf("%s",&*(p+qnt-1)->nome);
-            printf("Insira uma idade: "); scanf("%d",&(p+qnt-1)->idade);
-            printf("Insira uma altura: "); scanf("%d",&(p+qnt-1)->altura);
+            printf("Insira um nome: ");
+            scanf("%s", &*(p + qnt - 1)->nome);
+            printf("Insira uma idade: ");
+            scanf("%d", &(p + qnt - 1)->idade);
+            printf("Insira uma altura: ");
+            scanf("%d", &(p + qnt - 1)->altura);
             printf("Inserido com sucesso!");
         }
     } while (continuar != '0');
     system("cls");
-    int i=0;
-    while(i<qnt){
-        printf("%i - %s\n",i,p->nome);
-        printf("Idade: %d\n",p->idade);
-        printf("Altura: %d\n",p->altura);
+    int i = 0;
+    while (i < qnt)
+    {
+        printf("%i - %s\n", i, p->nome);
+        printf("Idade: %d\n", p->idade);
+        printf("Altura: %d\n", p->altura);
         printf("\n");
         i++;
         p++;
     }
-    
+
     free(p);
     return 0;
 }
