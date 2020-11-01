e1b2cbc90364f6fccf284d1174e4f1a3278b1570
Eduardo Nicoletti Borges <eduardonicoletti@mertins.com.br>
2020-11-01 18:29:57 -0300

    8 pronto

--- Semana 3/Exercicio 8/exercicio8.c
+++ Semana 3/Exercicio 8/exercicio8.c
@@ -4,9 +4,38 @@
 
 int main()
 {
-    char palavras[80];
-    printf("Insira as palavras separadas por '-' :\n");
-    scanf("%s", palavras);
+    char linha[80], *palavras = malloc(sizeof(char));
+    int letra = 0, space = 1;
+
+    printf("Insira até 80 caracteres:\n");
+    fgets(linha, 80, stdin);
+    for (int i = 0; i < strlen(linha); i++)
+    {
+        if (linha[i] == ' ' && space == 0)
+        {
+            space = 1;
+            palavras = (char *) realloc(palavras, sizeof(palavras) + sizeof(char));
+            palavras[letra] = '-';
+            letra++;
+        }
+        else if(linha[i] != ' ')
+        {
+            space = 0;
+            palavras = (char *) realloc(palavras, sizeof(palavras) + sizeof(char));
+            palavras[letra] = linha[i];
+            letra++;
+        }
+    }
+    for (int i = 0; i < letra; i++)
+    {
+        if(palavras[i] != '-'){
+            printf("%c", palavras[i]);
+        }else{
+            printf("\n");
+        }
+    }
     
+    free(palavras);
     return 0;
 }
+
