a8d6178c16224ac62a08b661612d24ae6bf05a2f
Eduardo Nicoletti Borges <eduardonicoletti@mertins.com.br>
2020-11-01 00:36:33 -0300

    drmemory e 8

---
+++ Semana 3/Exercicio 8/exercicio8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char palavras[80];
+    printf("Insira as palavras separadas por '-' :\n");
+    scanf("%s", palavras);
+    
+    return 0;
+}
